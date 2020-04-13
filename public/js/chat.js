fdd91bbb43c83f46909db50c53d57ba0a094bc0b
JoeRyan4 <56027134+JoeRyan4@users.noreply.github.com>
2020-04-13 23:22:00 +0100

    chat.js

--- public/js/chat.js
+++ public/js/chat.js
@@ -15,16 +15,27 @@ const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 // Options
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
 
-const autoscroll = () =>  {
-    //new message
-    const $newMessage =$messages.lastElementChild
+const autoscroll = () => {
+    // New message element
+    const $newMessage = $messages.lastElementChild
 
-
-    //height of message
-    const newMessageStyles = getComputedStyles($newMessage)
+    // Height of the new message
+    const newMessageStyles = getComputedStyle($newMessage)
     const newMessageMargin = parseInt(newMessageStyles.marginBottom)
     const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
 
+    // Visible height
+    const visibleHeight = $messages.offsetHeight
+
+    // Height of messages container
+    const containerHeight = $messages.scrollHeight
+
+    // How far have I scrolled?
+    const scrollOffset = $messages.scrollTop + visibleHeight
+
+    if (containerHeight - newMessageHeight <= scrollOffset) {
+        $messages.scrollTop = $messages.scrollHeight
+    }
 }
 
 socket.on('message', (message) => {
